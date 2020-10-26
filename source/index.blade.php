f0fb90c5597cb19e53bd32ea43267cd5178d2811
Kunal Shah <G559942@GENMILLS.COM>
2020-10-26 20:19:51 +0530

    Added the newer deducting and earning point images and edit the index

--- source/index.blade.php
+++ source/index.blade.php
@@ -19,7 +19,7 @@ So your job is going to be to start pulling this information together from diffe
 For CODE HUNT,  your team will need to use data from various databases to solve the puzzle. 
 To solve the puzzle you are given 4 clues (check puzzle tab)
 Solving each clue will give you a unique secret key. Using the secret key you will get database information from VAULT. This information will help connect to database and pull below information regarding the following 
-</ul>
+
 <ul>
 <li>Employee</li>
 <li>Location</li> 
@@ -28,14 +28,20 @@ Solving each clue will give you a unique secret key. Using the secret key you wi
 Relation between Employee, location, transport. Each clue has only one unique solution, and each can be solved using simple logical processes (i.e. educated guesses are not required).
 Clues can either provide you and your team a valid association (the subjects are linked) or dissociation (the subjects are not linked).
 </ul>
+</ul>
 <li>Expected Solution</li>
 <ul>
-We expect an Automated/Programed solution that would take input as relation and output the covidiot.
-Just in Time Bonus Challenge.
+We expect an Automated/Programmed solution that would take input as the values of relation table and return all the relation between Employee, Transport and Zone.
+</ul>
+
+<li>Just in Time Bonus Challenge.</li>
+<ul>
 Every 30 minutes code hunt team will email you bonus question. 
 Take the solution to the question and go retrieve a go/unwrap token.
 The Token is a one time use, so only one team will be able to claim the points. If you’re not in time, go/unwrap will give you an error.
 First team to use the unwrap token and provide the value to the Code Hunt Team will earn bonus points!
+The Codehunt team will be unbiased while selecting podiums in case of close calls or even ties and the Code Hunt committee's call will be the final one.
+</ul>
 </ul>
 </ul>
 @endsection
