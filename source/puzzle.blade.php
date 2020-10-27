221b21bdd68d92efd38b391e3056ce49bcaf4eed
Kunal Shah <G559942@GENMILLS.COM>
2020-10-27 10:42:26 +0530

    Added the clue and bonus clue details under puzzle tab

--- source/puzzle.blade.php
+++ source/puzzle.blade.php
@@ -14,4 +14,28 @@
             <p>No posts to show.</p>
         @endforelse
     </ul>
+
+    <h2> NOTE </h2>
+    <ul>
+    <li>For Clues:</li>
+    <ul><li>
+	The SPOC will email the CodeHunt Team using the following format. <br>
+	<b>Subject:</b> [Clue#] - [Team Name]<br>
+	<b>Message Body:</b> [Keep it empty]<br>
+	
+	<b>Example:</b> Clue 1 - Apocalypse 
+    </li></ul>
+	
+	<li>For Bonus Puzzle: </li>
+    <ul><li>
+	The SPOC will email the CodeHunt Team using the following format.<br>
+	<b>Subject:</b> [BonusPuzzle#] - [Team Name]<br>
+	<b>Message Body:</b> [Keep it empty]<br>
+	
+	<b>Example:</b> Bonus Puzzle 1 - Apocalypse</li></ul>
+
+    <li><i>CODEHUNT TEAM: Kunal Shah, Harshal Patani, Kapil Kumar Trivedi</i></li>
+    </ul>
+    Any format apart from this won't be considered or evaluated.
+
 @endsection
